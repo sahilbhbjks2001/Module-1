904518946515efdd0666f25e152a4e760e3948ce
sahilbhbjks2001 <75977428+sahilbhbjks2001@users.noreply.github.com>
2025-04-26 16:49:21 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,12 @@
+x=input('enter the first number :')
+y=input('enter the second number :')
+x=float(x)
+y=float(y)
+a=x+y
+s=x-y
+m=x*y
+d=x/y
+print('Addition : \t',a)
+print('Subtraction : \t',s)
+print('Multiplication : \t',m)
+print('Division :',d)
